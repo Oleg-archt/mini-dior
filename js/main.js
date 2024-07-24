4fbe2d9392d46bc991853c4c0b18942cb4fd8390
Oleg-archt <oleg23olee@gmail.com>
2024-07-24 18:49:12 +0300

    c

--- js/main.js
+++ js/main.js
@@ -16,11 +16,10 @@ const swiper = new Swiper(".top__swiper", {
   },
 });
 
-const swiperAbout = new Swiper(".about__slider", {
+var swiperAbout = new Swiper(".about__slider", {
   slidesPerView: 4,
   spaceBetween: 20,
   freeMode: true,
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
